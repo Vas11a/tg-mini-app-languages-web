34018b1407aee1f088a8480bcc1abfe9952a62c6
Vasyl <vasapanov721@gmail.com>
2026-02-17 21:47:23 +0100

    upd: search compontnt ui

--- src/componetns/search/SearchComponent.tsx
+++ src/componetns/search/SearchComponent.tsx
@@ -6,11 +6,11 @@ const SearchComponent = () => {
     <div className="relative flex justify-between ">
       <input
         type="text"
-        placeholder="Search deck title"
-        className="w-full border-2 смо border-primary/50 pr-7 rounded-lg text-[16px]  p-2 focus:outline-primary/80"
+        placeholder="Search"
+        className="w-full pr-7 bg-white focus:outline-0 focus:bg-primary/5 placeholder:text-primary/50 shadow-[0_1px_2px_rgba(0,0,0,0.04),0_4px_10px_rgba(0,0,0,0.04)] rounded-lg text-[16px]  p-2.5"
       />
       <SearchIcon
-        className="w-auto h-5 absolute opacity-80 top-3 right-2"
+        className="w-auto h-6 absolute opacity-80 top-2.5 right-2"
         color="var(--color-primary)"
       />
     </div>
