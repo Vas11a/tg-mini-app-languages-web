b2f045d4f769e109d2e89962c5e29b0364a000a3
Vasyl <vasapanov721@gmail.com>
2026-02-18 10:33:51 +0100

    upd: created mvp of home page design

--- src/componetns/search/SearchComponent.tsx
+++ src/componetns/search/SearchComponent.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { SearchIcon } from "../../images/SearchIcon";
+import { WorldIcon } from "../../images/WorldIcon";
+import { BarChartIcon } from "../../images/BarChartIcon";
 
 const SearchComponent = () => {
   return (
-    <div className="relative flex justify-between ">
-      <input
-        type="text"
-        placeholder="Search"
-        className="w-full pr-7 bg-white focus:outline-0 focus:bg-primary/5 placeholder:text-primary/50 shadow-[0_1px_2px_rgba(0,0,0,0.04),0_4px_10px_rgba(0,0,0,0.04)] rounded-lg text-[16px]  p-2.5"
-      />
-      <SearchIcon
-        className="w-auto h-6 absolute opacity-80 top-2.5 right-2"
-        color="var(--color-primary)"
-      />
+    <div className="p-2 flex flex-col gap-2 rounded-2xl bg-primary/10">
+      <div className="relative flex justify-between ">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full pr-7 text-dark bg-white focus:outline-0 focus:border focus:border-primary  placeholder:text-gray-300 shadow-[0_1px_2px_rgba(0,0,0,0.04),0_4px_10px_rgba(0,0,0,0.04)] rounded-lg text-[16px]  p-2.5 py-2"
+        />
+        <SearchIcon
+          className="w-auto h-6 absolute opacity-80 top-2 right-2"
+          color="var(--color-primary)"
+        />
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="flex-1 bg-white text-dark p-2 py-1.5 flex items-center gap-2 rounded-lg text-[14px]   shadow-[0_1px_2px_rgba(0,0,0,0.04),0_4px_10px_rgba(0,0,0,0.04)]">
+          <WorldIcon className="w-auto h-4.5" color="var(--color-primary)" />
+          <div>All languaged</div>
+        </div>
+        <div className="flex-1 bg-white text-dark p-2 py-1.5 flex items-center gap-2 rounded-lg text-[14px]   shadow-[0_1px_2px_rgba(0,0,0,0.04),0_4px_10px_rgba(0,0,0,0.04)]">
+          <BarChartIcon className="w-auto h-4.5" color="var(--color-primary)" />
+          <div>All levels</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/modules/deck_list/DeckListModule.tsx
+++ src/modules/deck_list/DeckListModule.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { ArrowIcon } from "../../images/ArrowIcon";
 
 interface Props {}
 
 const DeckListModule = ({}: Props) => {
   return (
     <div className="flex-1 overflow-y-auto min-h-0 flex flex-col gap-2">
-      <div></div>
+      <div className="mt-20 flex flex-col">
+        <div className="text-primary/40 text-center uppercase font-bold text-[30px]">
+          No decks yet
+        </div>
+        <div className="text-primary/60 text-center">
+          Create your first deck to start learning
+        </div>
+      </div>
     </div>
   );
 };
